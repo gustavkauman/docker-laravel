42c758bc20a8ffbe511f9a1eb818c596269d991a
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-07-21 22:24:07 +0200

    Add composer, #11

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt-get -y -qq install curl php7.4-fpm php-curl php-bcmath php-json php-mbstring php-mysql php-xml php-xmlrpc php-zip php-xdebug vim
+apt-get -y -qq install curl php7.4-fpm php-curl php-bcmath php-json php-mbstring php-mysql php-xml php-xmlrpc php-zip php-xdebug vim && \
+bash /var/tmp/install-composer.sh
